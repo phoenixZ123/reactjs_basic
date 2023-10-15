bc6c5b200628699e08d0efa8e4dde0a21905010d
phu nov <phuephue1125@gmail.com>
2023-10-15 13:04:54 +0630

    useRef

--- src/components/PostForm/Form.js
+++ src/components/PostForm/Form.js
@@ -1,29 +1,32 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './form.css'
 export const Form = ({ children, addPost }) => {
-  const [name, setName] = useState("");
+  // const [name, setName] = useState("");
 
+  const data = useRef();
   const CreatePost = (e) => {
     e.preventDefault();
+    console.log(data.current.value);
+    // let post = {
+    //   id: Math.floor(Math.random(1 * 999)),
+    //   name: name
+    // }
+    // addPost(post);
+    // resetForm();
 
-    let post = {
-      id: Math.floor(Math.random(1 * 999)),
-      name: name
-    }
-    addPost(post);
-    resetForm();
   }
 
   const resetForm = () => {
-    setName("")
+    // setName("")
+    console.log("success");
   }
   return (
     <form className='post-form' onSubmit={CreatePost}>
       <h3>Post Form</h3>
-      {name}
+
       <div className='form-control'>
         <label>Username</label>
-        <input type='text' onChange={(e) => setName(e.target.value)} value={name}></input>
+        <input type='text' ref={data}></input>
       </div>
 
       <div className='form-control'>
